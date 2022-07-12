00c1763345276dae8cf952bad1977c359fc398c1
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2022-07-12 12:58:17 -0400

    testing

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v ", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Hearts" {
+		t.Errorf("Expected last card to be King of Hearts, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v ", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
+}
